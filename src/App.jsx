69497624341ddee84bcabd34ab4fda1d85a1d9a6
Dakoda hitt <dakoda@MacBook-Air.local>
2024-04-02 19:43:00 -0600

    test

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { puppyList } from './data.js';
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const handleClick = (puppyId) => {
+    setFeatPupId(puppyId);
+  };
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log("featuredPup: ", featuredPup);
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+            {/* Add more details as needed */}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
